2108aa34a3b3261caf253e61ab63e50f57cbda57
Matthew Nowakowski <nowakowskim98@gmail.com>
2025-12-18 13:53:08 -0500

    Update useSyncedState to also manage initial queries

--- ui/src/hooks/useSyncedState.ts
+++ ui/src/hooks/useSyncedState.ts
@@ -1,16 +1,30 @@
 import { useContext, useEffect } from 'react'
-import { useQueryClient } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import useWebSocket from 'react-use-websocket'
 
 import settingsContext from '../settingsContext'
 
 import type StateUpdate from '../models/stateUpdate'
 
-export default function useSyncedState<DataType>(typeKey: string) {
+export default function useSyncedState<DataType>(typeKey: string, queryOptions?:{
+    queryUrl?: URL | string,
+    responseTransformer?: (response: Response) => Promise<DataType>
+}) {
+    const baseUrl = useContext(settingsContext).hostUrl
     const queryClient = useQueryClient()
 
-    const url = new URL('/sync', useContext(settingsContext).hostUrl).toString()
-    const { lastJsonMessage } = useWebSocket<StateUpdate<DataType>>(url, {
+    const query = useQuery<DataType>({
+        queryKey: [typeKey],
+        queryFn: async () => {
+            if(queryOptions?.queryUrl == undefined) return null
+            const response = await fetch(new URL(queryOptions.queryUrl, baseUrl))
+            if (queryOptions.responseTransformer) return await queryOptions.responseTransformer(response)
+            return await response.json()
+        }
+    })
+
+    const syncUrl = new URL('/sync', baseUrl).toString()
+    const { lastJsonMessage } = useWebSocket<StateUpdate<DataType>>(syncUrl, {
         share: true,
         shouldReconnect: () => true,
     })
@@ -20,4 +34,6 @@ export default function useSyncedState<DataType>(typeKey: string) {
         if (lastJsonMessage.typeKey != typeKey) return
         queryClient.setQueryData([typeKey], lastJsonMessage.data)
     }, [lastJsonMessage, queryClient, typeKey])
+
+    return query
 }
