4d62f810da1dd52e4082d246ad57b32f7eff41b5
Matthew Nowakowski <nowakowskim98@gmail.com>
2026-01-12 16:11:38 -0500

    Rewrote navbar, added theme properties

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -8,13 +8,11 @@ import SettingsContext from './settingsContext';
 
 export default function App() {
   return <SettingsContext value={{hostUrl: new URL('http://localhost/')}}>
-    <div className='navbar'>
-      <nav className="me-auto">
-        <a href="/fileplayer">File Player</a>
-        <a href="/soundfiles">Sound Files</a>
-        <a href="/settings">Settings</a>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <a href="/fileplayer">File Player</a>
+      <a href="/soundfiles">Sound Files</a>
+      <a href="/settings">Settings</a>
+    </nav>
 
     <Routes>
       <Route path='fileplayer' element={<FilePlayerContext />} />
